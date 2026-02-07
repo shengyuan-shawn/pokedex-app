58fc63ec90bb720756824684a1e79a6af4615526
ShengYuan-Shawn <shengyuan1103@gmail.com>
2026-02-07 23:06:08 +0800

    update github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { CssBaseline } from '@mui/material'
 import App from './App.jsx'
 
