fc9b2cfccfdd8b55981771e128982c67b1204005
ShengYuan-Shawn <shengyuan1103@gmail.com>
2026-02-07 23:21:23 +0800

    updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom';
-import { CssBaseline } from '@mui/material'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter basename="/pokedex-app">
     <CssBaseline />
     <App />
   </BrowserRouter>,
-)
+);
